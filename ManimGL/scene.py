74b949fd201d28e60a146b9e07fed40bea3a2be1
Lept0n51618 <anishgiri163@gmail.com>
2024-12-27 23:16:04 +0530

    Add new scenes for square-to-circle transformation and text transformation; update .gitignore

--- ManimGL/scene.py
+++ ManimGL/scene.py
@@ -11,3 +11,25 @@ class SquareToCircle(Scene):
         self.wait()
         self.play(ReplacementTransform(square, circle))
         self.wait()
+        
+# Transform square to circle
+class SquareToCircle2(Scene):
+    def construct(self):
+        square = Square()
+        square.set_fill(BLUE, opacity=0.5)
+        square.set_stroke(BLUE_E, width=4)
+        circle = Circle()
+
+        self.play(ShowCreation(square))
+        self.wait()
+        self.play(ReplacementTransform(square, circle))
+        self.wait()
+
+# Transform text
+class TextTransform(Scene):
+    def construct(self):
+        text1 = Text("Hello World")
+        text2 = Text("Goodbye World")
+        self.play(Write(text1))
+        self.wait()
+        self.play(Transform(text1, text2))
